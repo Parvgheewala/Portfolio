48ee18fb4f62ba753679addb99d734b84383d7b7
Parvgheewala <gheewalaparv@gmail.com>
2025-08-01 12:57:40 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolioOne/', // use your repo name
   plugins: [react()],
+  base: '/', // Ensures Vercel finds assets correctly
 });
